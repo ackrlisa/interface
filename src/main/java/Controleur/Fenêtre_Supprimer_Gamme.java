27ddfe3f599cce9632da2129989b71bddd464337
ackrlisa <lisa.acker67@gmail.com>
2025-05-04 13:52:37 +0200

    Merge origin/master
    # Conflicts:
    #	src/main/java/com/mycompany/projet_atelier/ProduitsFinis.java

--- src/main/java/Controleur/Fenêtre_Supprimer_Gamme.java
+++ src/main/java/Controleur/Fenêtre_Supprimer_Gamme.java
@@ -21,4 +21,13 @@ public class Fenêtre_Supprimer_Gamme {
         stage.setTitle("Fenêtre supprimer gamme");
         stage.setScene(supprimergamme.getFenêtre_Supprimer_Gamme()); //Appelle la méthode d'instance de la fenêtre accueil
         stage.show(); 
+        
+        
+        supprimergamme.getBtnHome().setOnAction(e -> {
+            stage.close();
+            // Crée une nouvelle fenêtre d'accueil
+            Stage accueilStage = new Stage();
+            new Fenêtre_Accueil(accueilStage); // Redirige vers la fenêtre d'accueil
+           });
+        
     }}
