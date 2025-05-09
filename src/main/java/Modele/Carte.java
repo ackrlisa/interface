a543e638b5590956001a0be16bbc5b4982209ab7
ackrlisa <lisa.acker67@gmail.com>
2025-05-09 12:58:32 +0200

    Merge origin/master
    # Conflicts:
    #	src/main/java/com/mycompany/projet_atelier/ProduitsFinis.java

---
+++ src/main/java/Modele/Carte.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modele;
+import java.util.ArrayList;
+/**
+ *
+ * @author lisaa
+ */
+public class Carte {
+ private ArrayList<Machine> machines;
+
+    public Carte() {
+        machines = new ArrayList<>();
+    }
+
+    public void ajoutMachine(String refMachine, String dMachine, float x, float y,float cout, ArrayList<Operation> listOperations, String type,Poste poste, ArrayList<Operateur> operateurMachine) {
+        machines.add(new Machine(refMachine,dMachine, x, y, cout, listOperations, type, poste, operateurMachine));
+    }
+       
+
+    // Méthode pour obtenir la liste des machines
+    public ArrayList<Machine> getMachines() {
+        return machines;
+    }
+    
+    
+     // Méthode pour chercher une machine par référence
+    public Machine getMachineParRef(String ref) {
+        for (Machine m : machines) {
+            if (m.getRefMachine().equals(ref)) {
+                return m;
+            }
+        }
+        return null;
+    }
+    
+    // Méthode pour mettre à jour la position d'une machine existante
+    public boolean updatePosition(String refMachine, float newX, float newY) {
+        Machine machine = getMachineParRef(refMachine);
+        if (machine != null) {
+            machine.setX(newX);
+            machine.setY(newY);
+            return true;
+        }
+        return false;
+    }
+
+}
+
