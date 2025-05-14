22003b4103baa2ffc36f2de47d0180817e9236d9
alicedeboever <alicedeboever@mac.lan>
2025-05-14 11:26:11 +0200

    Merge origin/master
    # Conflicts:
    #	src/main/java/Modele/Machine.java
    #	src/main/java/Vue/Créer_Gamme.java

--- src/main/java/com/mycompany/projet_atelier_interface/App.java
+++ src/main/java/com/mycompany/projet_atelier_interface/App.java
@@ -1,14 +1,8 @@
 package com.mycompany.projet_atelier_interface;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import Controleur.Fenêtre_Accueil;
-import Controleur.Fenêtre_Calcul_Duree_Gamme ;
-import Controleur.Fenêtre_Affichage_Poste ; 
-import Controleur.Fenêtre_CarteFx;
 import Modele.ModèleCarte;
 
 /**
@@ -18,12 +12,6 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        //var javaVersion = SystemInfo.javaVersion();
-        //var javafxVersion = SystemInfo.javafxVersion();
-        //var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        //var scene = new Scene(new StackPane(label), 640, 480);
-        //stage.setScene(scene);
-        //stage.show();
         ModèleCarte modeleCarte=new ModèleCarte();
         new Fenêtre_Accueil(stage,modeleCarte); //Lance le controleur
         
